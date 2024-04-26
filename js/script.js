9b4c4cf2ab09fad7b3e5e1d68aa29ed3f6c38488
horb7 <1834036790@qq.com>
2024-04-26 15:17:09 +0800

    Site updated: 2024-04-26 15:17:08

--- js/script.js
+++ js/script.js
@@ -1,58 +1,39 @@
-// 返回顶部 显示网页阅读进度
-window.onscroll = percent; // 执行函数
-// 页面百分比
-function percent() {
-  let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
-    b =
-      Math.max(
-        document.body.scrollHeight,
-        document.documentElement.scrollHeight,
-        document.body.offsetHeight,
-        document.documentElement.offsetHeight,
-        document.body.clientHeight,
-        document.documentElement.clientHeight
-      ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
-    result = Math.round((a / b) * 100), // 计算百分比
-    btn = document.querySelector("#percent"); // 获取图标
+//js有一个小问题：就是只要鼠标滚动不论哪里都会响应，即便你滚动的是子元素
 
-  result <= 99 || (result = 99), (btn.innerHTML = result);
-}
+//2022.9.11 已修复，需要jq，请自行引入
+document.getElementById("name-container").setAttribute("style", "display:none");
 
-document.getElementById("page-name").innerText = document.title.split(" | 安知鱼")[0];
+var position = $(window).scrollTop();
 
-window.scrollCollect = () => {
-    return btf.throttle(function (e) {
-      const currentTop = window.scrollY || document.documentElement.scrollTop
-      const isDown = scrollDirection(currentTop)
-      if (currentTop > 56) {
-+           $header.classList.add('is-top-bar')
-        if (isDown) {
-          if ($header.classList.contains('nav-visible')) $header.classList.remove('nav-visible')
-          if (isChatBtnShow && isChatShow === true) {
-            chatBtnHide()
-            isChatShow = false
-          }
-        } else {
-          if (!$header.classList.contains('nav-visible')) $header.classList.add('nav-visible')
-          if (isChatBtnHide && isChatShow === false) {
-            chatBtnShow()
-            isChatShow = true
-          }
-        }
-        $header.classList.add('nav-fixed')
-        if (window.getComputedStyle($rightside).getPropertyValue('opacity') === '0') {
-          $rightside.style.cssText = 'opacity: 0.8; transform: translateX(-58px)'
-        }
-      } else {
-        if (currentTop === 0) {
--             $header.classList.remove('nav-fixed', 'nav-visible')
-+             $header.classList.remove('is-top-bar')
-        }
-        $rightside.style.cssText = "opacity: ''; transform: ''"
-      }
+$(window).scroll(function () {
 
-      if (document.body.scrollHeight <= innerHeight) {
-        $rightside.style.cssText = 'opacity: 0.8; transform: translateX(-58px)'
-      }
-    }, 200)()
+  var scroll = $(window).scrollTop();
+
+  if (scroll > position) {
+
+
+    document.getElementById("name-container").setAttribute("style", "");
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
+
+  } else {
+
+
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+    document.getElementById("name-container").setAttribute("style", "display:none");
+
   }
+
+  position = scroll;
+
+});
+function scrollToTop(){
+    document.getElementsByClassName("menus_items")[1].setAttribute("style","");
+    document.getElementById("name-container").setAttribute("style","display:none");
+    btf.scrollToDest(0, 500);
+}
+//修复没有弄右键菜单的童鞋无法回顶部的问题
+document.getElementById("page-name").innerText = document.title.split(" | Ariasakaの小窝")[0];
+/*这里是去掉你的网站全局名称的设置，如果你不需要去掉，你可以写成：
+document.getElementById("page-name").innerText=document.title
+
+或者把你的网站的分隔符和全局网站名称加上去*/
